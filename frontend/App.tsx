8793e482e638c29d6548d767da28587187986fb1
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2026-03-10 19:11:40 +0700

    feat(app): update main app logic

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,37 @@
+// App.tsx
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/app/AppNavigator";
 import Toast from "react-native-toast-message";
+import { useEffect, useRef } from 'react';
+import * as Notifications from 'expo-notifications';
+import { registerForPushNotifications } from './src/services/notification.api';
 
 export default function App() {
+  const notificationListener = useRef<Notifications.EventSubscription | null>(null);
+  const responseListener = useRef<Notifications.EventSubscription | null>(null);
+
+  useEffect(() => {
+    registerForPushNotifications(); 
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      notification => {
+        console.log('Received:', notification);
+      }
+    );
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      response => {
+        const data = response.notification.request.content.data;
+        console.log('Tapped:', data);
+      }
+    );
+
+    return () => {
+      notificationListener.current?.remove();
+      responseListener.current?.remove();
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigator />
