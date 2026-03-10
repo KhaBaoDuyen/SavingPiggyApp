c0936740ecbb10199dbc3c6b6d08390f9df865a5
KhaBaoDuyen <143988268+KhaBaoDuyen@users.noreply.github.com>
2026-03-10 19:11:17 +0700

    fix(profile): update profile controller logic

--- backend/src/modules/profile/profile.controller.js
+++ backend/src/modules/profile/profile.controller.js
@@ -2,19 +2,11 @@ const profileService = require("./profile.service");
 
 exports.getProfile = async (req, res) => {
   try {
-
     const userId = req.user.userId;
-
     const profile = await profileService.getProfile(userId);
-
-    console.log("PROFILE:", profile);
-
     res.json(profile);
-
   } catch (error) {
-
     console.log("PROFILE ERROR:", error);
-
     res.status(400).json({
       message: error.message,
     });
@@ -23,11 +15,8 @@ exports.getProfile = async (req, res) => {
 
 exports.updateProfile = async (req, res) => {
   try {
-
     const userId = req.user.userId;
-
     const { name, birthDate, targetAmount, avatar } = req.body;
-
     const user = await profileService.updateProfile(
       userId,
       name,
@@ -35,7 +24,6 @@ exports.updateProfile = async (req, res) => {
       targetAmount,
       avatar
     );
-
     res.json({
       message: "Cập nhật thành công",
       user,
@@ -46,6 +34,5 @@ exports.updateProfile = async (req, res) => {
     res.status(400).json({
       message: error.message,
     });
-
   }
 };
